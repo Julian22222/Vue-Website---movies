e9b41c7a1235525e54badf1af279ba8c6d10df4f
Julian22222 <julik.golovenj@gmail.com>
2023-05-20 23:49:40 +0100

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
-// const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: "/list-with-products/",
-  // transpileDependencies: true,
-  // devServer: {
-  //   proxy: "https://movies-ypff.onrender.com",
-  // },
+  transpileDependencies: true,
+  devServer: {
+    proxy: "https://movies-ypff.onrender.com",
+  },
 });
